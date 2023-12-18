a652843f54dfe24828a9091cadb118d3a1789371
AtharvaD10 <deshpandeatharva10@yahoo.com>
2023-12-18 23:54:23 +0530

    SignIn and SignUp is created

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='absolute px-8 py-2 bg-gradient-to-b from-black  z-10'>
+        <img
+        className='w-44 ml-6 mt-5 bg-gradient-to-b from-black'
+         src='https://assets.nflxext.com/en_us/layout/ecweb/common/logo-shadow2x.png'
+        alt='netfilxlogo' />
+    </div>
+  )
+}
+
+export default Header
